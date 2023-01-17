0ad6f8603707b42e34b237793db2c7d3d171a93d
MehdiZayani <77887730+MehdiZayani@users.noreply.github.com>
2023-01-17 10:38:10 +0100

    first commit

---
+++ pages/echantillon.jsx
@@ -0,0 +1,96 @@
+import Image from "next/image"
+import Styles from "../styles/echantillon.module.css"
+import image1 from "../images/echantillon/HG-MAT-S029-scaled.jpg"
+export default function echant() {
+    return(
+        <>
+        <div className={Styles.echant}>
+            <div className={Styles.card}>
+                <Image className={Styles.cardimg} src={image1} height={300}width={300}style={{border: "solid 2px black"}}/>
+                <p className={Styles.cardtitle}>High Gloss S029</p>
+                <p className={Styles.cardpara}>Epaisseur: 18mm</p>
+            </div>
+            <div className={Styles.card}>
+                <Image className={Styles.cardimg} src={image1} height={300}width={300}style={{border: "solid 2px black"}}/>
+                <p className={Styles.cardtitle}>High Gloss S029</p>
+                <p className={Styles.cardpara}>Epaisseur: 18mm</p>
+            </div>
+            <div className={Styles.card}>
+                <Image className={Styles.cardimg} src={image1} height={300}width={300}style={{border: "solid 2px black"}}/>
+                <p className={Styles.cardtitle}>High Gloss S029</p>
+                <p className={Styles.cardpara}>Epaisseur: 18mm</p>
+            </div>
+            <div className={Styles.card}>
+                <Image className={Styles.cardimg} src={image1} height={300}width={300}style={{border: "solid 2px black"}}/>
+                <p className={Styles.cardtitle}>High Gloss S029</p>
+                <p className={Styles.cardpara}>Epaisseur: 18mm</p>
+            </div>
+            <div className={Styles.card}>
+                <Image className={Styles.cardimg} src={image1} height={300}width={300}style={{border: "solid 2px black"}}/>
+                <p className={Styles.cardtitle}>High Gloss S029</p>
+                <p className={Styles.cardpara}>Epaisseur: 18mm</p>
+            </div>
+            <div className={Styles.card}>
+                <Image className={Styles.cardimg} src={image1} height={300}width={300}style={{border: "solid 2px black"}}/>
+                <p className={Styles.cardtitle}>High Gloss S029</p>
+                <p className={Styles.cardpara}>Epaisseur: 18mm</p>
+            </div>
+            <div className={Styles.card}>
+                <Image className={Styles.cardimg} src={image1} height={300}width={300}style={{border: "solid 2px black"}}/>
+                <p className={Styles.cardtitle}>High Gloss S029</p>
+                <p className={Styles.cardpara}>Epaisseur: 18mm</p>
+            </div>
+            <div className={Styles.card}>
+                <Image className={Styles.cardimg} src={image1} height={300}width={300}style={{border: "solid 2px black"}}/>
+                <p className={Styles.cardtitle}>High Gloss S029</p>
+                <p className={Styles.cardpara}>Epaisseur: 18mm</p>
+            </div>
+            <div className={Styles.card}>
+                <Image className={Styles.cardimg} src={image1} height={300}width={300}style={{border: "solid 2px black"}}/>
+                <p className={Styles.cardtitle}>High Gloss S029</p>
+                <p className={Styles.cardpara}>Epaisseur: 18mm</p>
+            </div>
+            <div className={Styles.card}>
+                <Image className={Styles.cardimg} src={image1} height={300}width={300}style={{border: "solid 2px black"}}/>
+                <p className={Styles.cardtitle}>High Gloss S029</p>
+                <p className={Styles.cardpara}>Epaisseur: 18mm</p>
+            </div>
+            <div className={Styles.card}>
+                <Image className={Styles.cardimg} src={image1} height={300}width={300}style={{border: "solid 2px black"}}/>
+                <p className={Styles.cardtitle}>High Gloss S029</p>
+                <p className={Styles.cardpara}>Epaisseur: 18mm</p>
+            </div>
+            <div className={Styles.card}>
+                <Image className={Styles.cardimg} src={image1} height={300}width={300}style={{border: "solid 2px black"}}/>
+                <p className={Styles.cardtitle}>High Gloss S029</p>
+                <p className={Styles.cardpara}>Epaisseur: 18mm</p>
+            </div>
+            <div className={Styles.card}>
+                <Image className={Styles.cardimg} src={image1} height={300}width={300}style={{border: "solid 2px black"}}/>
+                <p className={Styles.cardtitle}>High Gloss S029</p>
+                <p className={Styles.cardpara}>Epaisseur: 18mm</p>
+            </div>
+            <div className={Styles.card}>
+                <Image className={Styles.cardimg} src={image1} height={300}width={300}style={{border: "solid 2px black"}}/>
+                <p className={Styles.cardtitle}>High Gloss S029</p>
+                <p className={Styles.cardpara}>Epaisseur: 18mm</p>
+            </div>
+            <div className={Styles.card}>
+                <Image className={Styles.cardimg} src={image1} height={300}width={300}style={{border: "solid 2px black"}}/>
+                <p className={Styles.cardtitle}>High Gloss S029</p>
+                <p className={Styles.cardpara}>Epaisseur: 18mm</p>
+            </div>
+            <div className={Styles.card}>
+                <Image className={Styles.cardimg} src={image1} height={300}width={300}style={{border: "solid 2px black"}}/>
+                <p className={Styles.cardtitle}>High Gloss S029</p>
+                <p className={Styles.cardpara}>Epaisseur: 18mm</p>
+            </div>
+            <div className={Styles.card}>
+                <Image className={Styles.cardimg} src={image1} height={300}width={300}style={{border: "solid 2px black"}}/>
+                <p className={Styles.cardtitle}>High Gloss S029</p>
+                <p className={Styles.cardpara}>Epaisseur: 18mm</p>
+            </div>
+        </div>
+        </>
+    )
+}
